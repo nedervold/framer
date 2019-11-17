7517f9eff04223005d9db367483e3c21671063a6
Eric Nedervold <nedervoldsoftware@gmail.com>
2019-11-16 23:09:53 -0800

    Refactoring; can now reformat without breaking Makefile.hs.

--- src/Framer/Makefile.hs
+++ src/Framer/Makefile.hs
@@ -5,30 +5,31 @@ module Framer.Makefile where
 
 import Data.ByteString (ByteString)
 import Data.ByteString.UTF8 (fromString)
-import Data.String.Interpolate (i)
 import Framer.Config
+import Text.Printf (printf)
 
 -- | TODO Tabs and backslashes get messed up, either on production or,
 -- more likely, when running hindent.
 makefileText :: Config -> ByteString
 makefileText Config {..} =
-  fromString
-    [i|.PHONY : clean reformat hlint run
-
-REFORMAT = hindent --line-length 76 --sort-imports
-
-run :
-        stack build
-        stack exec #{projectName}-exe
-
-hlint :
-        hlint app src test
-
-reformat :
-        find app src test -name '*.hs' -exec $(REFORMAT) \{} \;
-
-clean : # reformat
-        stack clean
-        find . -name '*~' -delete
-        find . -name '#*' -delete
-|]
+  fromString $
+  unlines
+    [ ".PHONY : clean reformat hlint run"
+    , ""
+    , "REFORMAT = hindent --line-length 76 --sort-imports"
+    , ""
+    , "run :"
+    , "\tstack build"
+    , printf "\tstack exec %s-exe" projectName
+    , ""
+    , "hlint :"
+    , "\thlint app src test"
+    , ""
+    , "reformat :"
+    , "\tfind app src test -name '*.hs' -exec $(REFORMAT) \\{} \\;"
+    , ""
+    , "clean : # reformat"
+    , "\tstack clean"
+    , "\tfind . -name '*~' -delete"
+    , "\tfind . -name '#*' -delete"
+    ]
